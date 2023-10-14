60b2dcddf0ce2cc683000ec2ca42cd25409ce728
Kirzem1 <146539002+Kirzem1@users.noreply.github.com>
2023-10-14 14:36:09 +0300

    Update HowMany.java

--- HowMany.java
+++ HowMany.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 public class HowMany {
         public static void main(String[] args) {
-// Создаем объект класса Scanner для чтения ввода с консоли
             Scanner scanner = new Scanner(System.in);
-
             System.out.println("Введите строку:");
-            String input = scanner.nextLine(); // Читаем введенную строку
-
-            int HowMany = HowMany(input); // Вызываем метод HowMany для подсчета слов
+            String input = scanner.nextLine();
+            int HowMany = HowMany(input); 
             System.out.println("Количество слов: " + HowMany);
         }
         private static int HowMany(String input) {
-            String[] words = input.split("\\s+"); // Делим строку на слова, используя пробел в качестве разделителя
-            return words.length; // Возвращаем количество слов
+            String[] words = input.split("\\s+");
+            return words.length;
         }
     }
 
